f7cc543412b2c1e81189137d7d3580b0df8b17c0
adcarmona <ancarmona@697-334-Combe.sf.k12.internal>
2015-01-06 11:12:54 -0800

    Finished Program (No Current Extensions)

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -46,19 +46,23 @@ public String pigLatin(String sWord)
 //precondition: sWord is a valid String of length greater than 0
 //postcondition: returns the pig latin equivalent of sWord
 {
-	if(findFirstVowel(sWord) == 0)
-	{
-		return sWord + "way";
-	}
-	if(sWord.substring(0,2) == "qu")
-	{
-		return sWord.substring(2,sWord.length()+1) + "qu";
-	}
 	if(findFirstVowel(sWord) == -1)
 	{
 		return sWord + "ay";
 	}
-	else
+	if(findFirstVowel(sWord) == 0)
+	{
+		return sWord + "way";
+	}
+	if(sWord.substring(0,2).equals("qu"))
+	{
+		return sWord.substring(2,sWord.length()) + "qu" + "ay";
+	}
+	if(findFirstVowel(sWord) > 0)
+	{
+		return sWord.substring(findFirstVowel(sWord), sWord.length()) + sWord.substring(0, findFirstVowel(sWord)) + "ay";
+	}
+	else 
 	{
 		return "ERROR!";
 	}
